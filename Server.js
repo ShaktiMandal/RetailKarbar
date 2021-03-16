1536976cad2a971c39512e6b277b3dc04b0efbc0
s2304 <34964652+s2304@users.noreply.github.com>
2021-03-16 22:45:50 +0530

    Setting custom header again

--- Server.js
+++ Server.js
@@ -91,18 +91,14 @@ App.use('/', (req, res) => {
 
     console.log("header is present", res.headersSent);
     if (process.env.NODE_ENV === "production" && req.isAuthenticated())
-    {      
-        res.status(200).json({
-
-            IsUserLoggedIn: true
-        }).sendFile(path.join(__dirname, './client/build/index.html'));;
+    {     
+        res.setHeader("Is-UserloggedIn","true"); 
+        res.status(200).sendFile(path.join(__dirname, './client/build/index.html'));;
     }
     else
     {
-         res.status(200).json({
-            
-            IsUserLoggedIn: false
-        }).sendFile(path.join(__dirname, './client/public/index.html'));;
+        res.setHeader("Is-UserloggedIn","false");
+         res.status(200).sendFile(path.join(__dirname, './client/public/index.html'));;
     }
 });
 
